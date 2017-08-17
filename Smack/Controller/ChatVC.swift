c2f95b97972b58f38bb551bab331320a00d1d8f7
kdoebr21 <kodeberic@yahoo.co.jp>
2017-08-17 13:41:24 +0900

    slideView

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by User on 2017/08/17.
+//  Copyright © 2017年 Yusuke Hirose. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    
+    @IBOutlet var MenuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        MenuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+   
+}
