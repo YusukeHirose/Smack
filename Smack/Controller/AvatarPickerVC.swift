72db788928018a8d1371a90cc0e2b2d82bc55770
kdoebr21 <kodeberic@yahoo.co.jp>
2017-09-05 23:14:12 +0900

    setAvatar

--- Smack/Controller/AvatarPickerVC.swift
+++ Smack/Controller/AvatarPickerVC.swift
@@ -14,6 +14,9 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
+    //Variables
+    var avatarType = AvatarType.dark
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +28,9 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell{
+            // AvatarCellファイル内のconfigureCellメソッドを呼び出す
+            cell.configureCell(index:indexPath.item, type: avatarType)
+            
             return cell
         }
         return AvatarCell()
@@ -40,6 +46,34 @@ class AvatarPickerVC: UIViewController,UICollectionViewDelegate,UICollectionView
     }
     
     @IBAction func segmentControlChanged(_ sender: Any) {
+        
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        }else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var numOfColumns : CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numOfColumns = 4
+        }
+        let spaceBetweenCells : CGFloat = 10
+        let padding : CGFloat = 40
+        let cellDimention = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        return CGSize(width: cellDimention, height: cellDimention)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        }else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func backPressed(_ sender: Any) {
--- Smack/View/AvatarCell.swift
+++ Smack/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImg: UIImageView!
@@ -18,6 +23,17 @@ class AvatarCell: UICollectionViewCell {
         
         setUpView()
         
+    }
+    
+    func configureCell(index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named:"dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImg.image = UIImage(named:"light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+            
+        }
     }
     
     func setUpView() {
