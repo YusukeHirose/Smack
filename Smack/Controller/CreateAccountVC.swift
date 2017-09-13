a772e6f6d087a625e6c16dd8be91b872a705ef38
kdoebr21 <kodeberic@yahoo.co.jp>
2017-09-13 16:23:39 +0900

    loggedIn

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -19,14 +19,18 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var userImg: UIImageView!
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     // Variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
-    
+    var bgColor : UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupView()
         // Do any additional setup after loading the view.
     }
     
@@ -35,11 +39,17 @@ class CreateAccountVC: UIViewController {
             userImg.image = UIImage(named:
                 UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && bgColor == nil {
+                userImg.backgroundColor = UIColor.lightGray
+            }
         }
     }
 
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
         guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else { return }
         guard let pass = passTxt.text , passTxt.text != "" else { return }
@@ -51,9 +61,10 @@ class CreateAccountVC: UIViewController {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in if success {
                     AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                         if success{
-                            print(UserDataService.instance.name,
-                                  UserDataService.instance.avatarName)
+                            self.spinner.isHidden = true
+                            self.spinner.stopAnimating()
                             self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object:nil)
                     }
                     })
             }
@@ -67,11 +78,36 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickBGColorPressed(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+        
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1 )
+        avatarColor = "[\(r), \(g), \(b), 1]"
+        UIView.animate(withDuration: 0.2) {
+            self.userImg.backgroundColor = self.bgColor
+        }
     }
     
     
     @IBAction func closePressed(_ sender:Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    func setupView(){
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+         emailTxt.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+         passTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        
+        //タップした時キーボードを閉じる処理を呼び出す
+        let tap = UITapGestureRecognizer(target:self, action: #selector(CreateAccountVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    //キーボードを閉じる処理
+    @objc func handleTap() {
+        view.endEditing(true)
     }
 
 }
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,13 @@ let BASE_URL = "https://slackcloneapp.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
+
+//Colors(placeholderの文字色)
+let smackPurplePlaceholder = #colorLiteral(red: 0.2588235294, green: 0.3294117647, blue: 0.7254901961, alpha: 0.5)
+
+//Notification Constants
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
 
 // Segues
 let TO_LOGIN = "toLogin"
@@ -31,4 +38,9 @@ let USER_EMAIL = "userEmail"
 // Headers
 let HEADER = [
     "Content-Type" : "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization":"Bearer \(AuthService.instance.authToken)",
+    "Content-Type": "application/json; charset=utf-8"
 ]
