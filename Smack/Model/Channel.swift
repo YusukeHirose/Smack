b16ec342a776981a554828767fa0168fd181958f
kdoebr21 <kodeberic@yahoo.co.jp>
2017-09-17 14:13:33 +0900

    channelAPI

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,20 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by User on 2017/09/13.
+//  Copyright © 2017年 Yusuke Hirose. All rights reserved.
+//
+
+import Foundation
+
+struct Channel : Decodable {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+//    public private(set) var _id: String!
+//    public private(set) var name: String!
+//    public private(set) var description: String!
+//    public private(set) var __v: Int?
+}
